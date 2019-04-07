a06a49310cea5df1fdb24647aa8e5d490dbe23fc
PanicKode <panicc.co@gmail.com>
2019-04-07 15:39:14 -0600

    New Setting And Bot Id Randomization Update
    added L6 PMC bots
    new setting added to setting.json and test.js to turn on or off panicrandom. turn off to enable default script from Head.
    
    Added all id randomization and stripping of usless UDP tags or location tags.
    
    BOTS STILL THROW ERRORS ON LABS?
    Im not sure what is causeing these errors. 25% of the time it works just fine?

--- Enut/_BOT_CREATION/test.js
+++ Enut/_BOT_CREATION/test.js
@@ -54,10 +54,7 @@ function randomString(length, chars) {
 //main script starts here
 (async (url) => {
 	
-	//PK CLEAR OLD FILE (ADD SETTING TO TURN OFF PRANDOM)
-	var emptyf = "";
-	fs.writeFileSync('Output.json', emptyf, 'utf8');
-	fs.writeFileSync('randomz.json', outJson, 'utf8');
+	
 
 	var Settings = JSON.parse(ReadJson('Settings.json'));
 	
@@ -68,6 +65,7 @@ function randomString(length, chars) {
 	var BossName	= Settings.BossName;
 	var toConsole	= Settings.DumpToConsoleOnly;
 	var saveSingle	= Settings.ClearFileB4dump;
+	var PanicRandom = Settings.PanicRandom; //PK
 	console.log("Settings:");
 	console.log("botType = " + botType + " / botDiff = " + botDiff + " / randomizeS= " + randomizeS + " / isBoss = " + isBoss + ((isBoss)?(" / BossName = " + BossName):"") + " / toConsole = " + toConsole + " / saveSingle = " + saveSingle);
 
@@ -79,8 +77,22 @@ function randomString(length, chars) {
 			listFiles[i] = Folders[i];
 	}
 // finished list
+
+//PK :: Clear Files Before Run
+if(PanicRandom == true){
+	var emptyf		= "";
+	fs.writeFileSync('randomz.json', emptyf, 'utf8');
+	fs.writeFileSync('Output.json', emptyf, 'utf8');
+}
 var count = 0;
 for(var i in listFiles){
+	
+	if(PanicRandom == true){
+		//PK Clear Output Between Bots
+		fs.writeFileSync('Output.json', emptyf, 'utf8');
+	}
+	
+	
 console.log("} Start creating for file: " + listFiles[i])
 	if(randomizeS){
 		//for savages only
@@ -105,6 +117,7 @@ console.log("} Start creating for file: " + listFiles[i])
 	}
 	var profileList = JSON.parse(ReadJson('BotProfile/' + listFiles[i]));
 	var outJson = ReadJson('Output.json');
+	var outJson2 = ReadJson('randomz.json');
 	var hloop = true; //added for hideout
 	
 	profileList = profileList.data[1];
@@ -131,30 +144,34 @@ console.log("} Start creating for file: " + listFiles[i])
 	var out='';
 	var UniqID = "bot_" + botType + "_" + botDiff + "" + setID();
 	
-//Should not edit header tags without matching to rest of bot
-//Removed for now. ids will remain : use panicRandom 
+	//Switch Added for PanicRandom
+	if(PanicRandom == false) {
 	
-	//var hash_eq = profileList.Inventory.equipment;
-	//var hash_st = profileList.Inventory.stash;
-	//var hash_qr = profileList.Inventory.questRaidItems;
-	//var hash_qs = profileList.Inventory.questStashItems;
-	//var new_hash_eq = UniqID + "_EQ";
-	//var new_hash_st = UniqID + "_ST";
-	//var new_hash_qr = UniqID + "_QR";
-	//var new_hash_qs = UniqID + "_QS";
-	//profileList.Inventory.equipment = new_hash_eq;
-	//profileList.Inventory.stash = new_hash_st;
-	//profileList.Inventory.questRaidItems = new_hash_qr;
-	//profileList.Inventory.questStashItems = new_hash_qs;
-
-	var prepareInventory = JSON.stringify(profileList.Inventory);
-
+		var hash_eq = profileList.Inventory.equipment;
+		var hash_st = profileList.Inventory.stash;
+		var hash_qr = profileList.Inventory.questRaidItems;
+		var hash_qs = profileList.Inventory.questStashItems;
+		var new_hash_eq = UniqID + "_EQ";
+		var new_hash_st = UniqID + "_ST";
+		var new_hash_qr = UniqID + "_QR";
+		var new_hash_qs = UniqID + "_QS";
+		profileList.Inventory.equipment = new_hash_eq;
+		profileList.Inventory.stash = new_hash_st;
+		profileList.Inventory.questRaidItems = new_hash_qr;
+		profileList.Inventory.questStashItems = new_hash_qs;
+		
+		var prepareInventory = JSON.stringify(profileList.Inventory);
+		prepareInventory = prepareInventory.replace(hash_eq, new_hash_eq).replace(hash_st, new_hash_st).replace(hash_qr, new_hash_qr).replace(hash_qs, new_hash_qs);
+	}
+	else {
+		var prepareInventory = JSON.stringify(profileList.Inventory);
+	}
+	
 	if(voice == "")
 		voice = profileList.Info.Voice;
 	if(prepareCustomization == "")
 		prepareCustomization = JSON.stringify(profileList.Customization) + ", ";
 
-	//prepareInventory = prepareInventory.replace(hash_eq, new_hash_eq).replace(hash_st, new_hash_st).replace(hash_qr, new_hash_qr).replace(hash_qs, new_hash_qs);
 	out += '{"_id": "' + UniqID + '", "aid": 0,"savage": null,"Info": {' + 
 	'"Nickname": "' + botName + '","LowerNickname": "","Side": "Savage","Voice": "' + voice + '","Level": 1,"Experience": 0,"RegistrationDate": 0,"GameVersion": "","AccountType": 0,"MemberCategory": 0,"lockedMoveCommands": false,"LastTimePlayedAsSavage": 0,' + 
 	'"Settings":{"Role": "' + botType + '","BotDifficulty": "' + botDiff + '","Experience": -1},"NeedWipe": false,"GlobalWipe": false,"NicknameChangeDate": 0},' + 
@@ -171,19 +188,43 @@ console.log("} Start creating for file: " + listFiles[i])
 	count++;//basic counter ;) - could help to set things up !
 	
 	//////////////PANICKODE /////////////////
-	//////////////START TO RANDOMIZE IDS (only working if output/randomz.jsn file is blank before start??)
+	//////////////START TO RANDOMIZE IDS /////////////////
+if (PanicRandom == true) {
 	var profileList2 = JSON.parse(ReadJson('Output.json'));
 	var out2='';
 	var prepareInventory2 = JSON.stringify(profileList2.Inventory);
 	
 	
-	//ID TO REPLACE
+	//Read, Collect and Randomize ID
+	//Strip Location Codes not needed
+	//Strip UDP -stack ammount
+
 		//MAIN ID
 		var mainid = profileList2.Inventory.items[0]._id;
 		var nmainid = randomString(24, 'abcdef1234567890');
 		prepareInventory2 = prepareInventory2.replace(new RegExp(mainid, 'g'), nmainid);
+		profileList2.Inventory.equipment = nmainid;
 		
-		//Collect Id Numbers and Generate New ones
+		//STASH ID
+		var stashid = profileList2.Inventory.stash;
+		var nstashid = randomString(24, 'abcdef1234567890');
+		prepareInventory2 = prepareInventory2.replace(new RegExp(stashid, 'g'), nstashid);
+		profileList2.Inventory.stash = nstashid;
+		
+		//Quest Raid
+		var questid = profileList2.Inventory.questRaidItems;
+		var nquestid = randomString(24, 'abcdef1234567890');
+		prepareInventory2 = prepareInventory2.replace(new RegExp(questid, 'g'), nquestid);
+		profileList2.Inventory.questRaidItems = nquestid;
+		
+		//Quest Inventory
+		var questid2 = profileList2.Inventory.questStashItems;
+		var nquestid2 = randomString(24, 'abcdef1234567890');
+		prepareInventory2 = prepareInventory2.replace(new RegExp(questid2, 'g'), nquestid2);
+		profileList2.Inventory.questStashItems = nquestid2;
+
+		
+		//Itterate Through Sub Items
 		for (var tmpKey2 in profileList2.Inventory.items) {	
 			//Pockets id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "Pockets") {
@@ -198,6 +239,11 @@ console.log("} Start creating for file: " + listFiles[i])
 				var FPWid = profileList2.Inventory.items[tmpKey2]._id;
 				var nFPWid = randomString(24, 'abcdef1234567890');
 				prepareInventory2 = prepareInventory2.replace(new RegExp(FPWid, 'g'), nFPWid);
+				//Remove location/UDP
+				var FPWS = '"slotId":"FirstPrimaryWeapon","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var FPWR = '"slotId":"FirstPrimaryWeapon"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(FPWS, 'g'), FPWR);
+				
 				//FirstPrimaryWeapon parent id
 				var FPWPid = profileList2.Inventory.items[tmpKey2].parentId;
 			}
@@ -208,6 +254,10 @@ console.log("} Start creating for file: " + listFiles[i])
 				prepareInventory2 = prepareInventory2.replace(new RegExp(TVid, 'g'), nTVid);
 				//TacticalVest parent id
 				var TVPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var TVS = '"slotId":"TacticalVest","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var TVR = '"slotId":"TacticalVest"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(TVS, 'g'), TVR);
 			}
 			//ArmorVest Id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "ArmorVest") {
@@ -216,6 +266,10 @@ console.log("} Start creating for file: " + listFiles[i])
 				prepareInventory2 = prepareInventory2.replace(new RegExp(AVid, 'g'), nAVid);
 				//ArmorVest parent id
 				var AVPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var AVS = '"slotId":"ArmorVest","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var AVR = '"slotId":"ArmorVest"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(AVS, 'g'), AVR);
 			}
 			//Backpack Id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "Backpack") {
@@ -224,6 +278,10 @@ console.log("} Start creating for file: " + listFiles[i])
 				prepareInventory2 = prepareInventory2.replace(new RegExp(BPid, 'g'), nBPid);
 				//Backpack parent id
 				var BPPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var BPS = '"slotId":"Backpack","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var BPR = '"slotId":"Backpack"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(BPS, 'g'), BPR);
 			}
 			//Headwear Id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "Headwear") {
@@ -231,7 +289,11 @@ console.log("} Start creating for file: " + listFiles[i])
 				var nHWid = randomString(24, 'abcdef1234567890');
 				prepareInventory2 = prepareInventory2.replace(new RegExp(HWid, 'g'), nHWid);
 				//Headwear parent id
-				var HWPid = profileList2.Inventory.items[tmpKey2].parentId;	
+				var HWPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var HWS = '"slotId":"Headwear","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var HWR = '"slotId":"Headwear"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(HWS, 'g'), HWR);
 			}
 			//FaceCover Id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "FaceCover") {
@@ -239,7 +301,11 @@ console.log("} Start creating for file: " + listFiles[i])
 				var nFCid = randomString(24, 'abcdef1234567890');
 				prepareInventory2 = prepareInventory2.replace(new RegExp(FCid, 'g'), nFCid);
 				//FaceCover parent id
-				var FCPid = profileList2.Inventory.items[tmpKey2].parentId;	
+				var FCPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var FCS = '"slotId":"FaceCover","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var FCR = '"slotId":"FaceCover"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(FCS, 'g'), FCR);
 			}
 			//Scabbard Id
 			if (profileList2.Inventory.items[tmpKey2].slotId == "Scabbard") {
@@ -249,49 +315,74 @@ console.log("} Start creating for file: " + listFiles[i])
 				//Scabbard parent id
 				var SCPid = profileList2.Inventory.items[tmpKey2].parentId;
 			}
+			//Eyewear Id
+			if (profileList2.Inventory.items[tmpKey2].slotId == "Eyewear") {
+				var EWid = profileList2.Inventory.items[tmpKey2]._id;
+				var nEWid = randomString(24, 'abcdef1234567890');
+				prepareInventory2 = prepareInventory2.replace(new RegExp(EWid, 'g'), nEWid);
+				//Scabbard parent id
+				var EWPid = profileList2.Inventory.items[tmpKey2].parentId;
+				//Remove location/UDP
+				var EWS = '"slotId":"Eyewear","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var EWR = '"slotId":"Eyewear"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(EWS, 'g'), EWR);
+			}
+			//Pockets
+			var str1 = profileList2.Inventory.items[tmpKey2].slotId;
+			var rgx1 = /pocket*/;
+			if (rgx1.test(str1)) {
+				var pktid = profileList2.Inventory.items[tmpKey2]._id;
+				var npkid = randomString(24, 'abcdef1234567890');
+				prepareInventory2 = prepareInventory2.replace(new RegExp(pktid, 'g'), npkid);
+				//Remove location/UDP
+				var pckname = profileList2.Inventory.items[tmpKey2].slotId;
+				console.log("pocket found:" + pckname);
+				var PKTS = '"slotId":"'+ pckname +'","location":{"x":0,"y":0,"r":"Horizontal","isSearched":true},"upd":{"StackObjectsCount":1}';
+				var PKTR = '"slotId":"'+ pckname +'","location":{"x":0,"y":0,"r":0}';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(PKTS, 'g'), PKTR);
+			}
+			//Slots
+			if (profileList2.Inventory.items[tmpKey2].slotId == "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "10") {
+				var Sltid = profileList2.Inventory.items[tmpKey2]._id;
+				var nSltid = randomString(24, 'abcdef1234567890');
+				prepareInventory2 = prepareInventory2.replace(new RegExp(Sltid, 'g'), nSltid);
+			}
+			//MODS
+			var str2 = profileList2.Inventory.items[tmpKey2].slotId;
+			var rgx2 = /mod_.*/;
+			if (rgx2.test(str2)) {
+				var modid = profileList2.Inventory.items[tmpKey2]._id;
+				var nmodid = randomString(24, 'abcdef1234567890');
+				prepareInventory2 = prepareInventory2.replace(new RegExp(modid, 'g'), nmodid);
+				//Remove location/UDP
+				var modname = profileList2.Inventory.items[tmpKey2].slotId;
+				console.log("mod found:" + modname);
+				var MODS = '"slotId":"'+ modname +'","location":{"x":0,"y":0,"r":0},"upd":{"StackObjectsCount":1}';
+				var MODR = '"slotId":"'+ modname +'"';
+				prepareInventory2 = prepareInventory2.replace(new RegExp(MODS, 'g'), MODR);
+				
+				
+			//Correct Remaining Location Codes
+			var LOCS = '"r":"Horizontal","isSearched":true';
+			var LOCR = '"r":0';
+			prepareInventory2 = prepareInventory2.replace(new RegExp(LOCS, 'g'), LOCR);
+				
+			}
 			
-			//Possible new ids needed
-			//Maby Still need Mod_**
-			//Maby Need to Random Cartridges
-			//maby need to random 1-10 (slotid:1-10)
-			//maby need to random pocket1-10
-			
+			// END Pull and Randomize
+				//cartridges? needs Testing
 		}
-		//output all ID Results - DEBUGGING
-		//console.log("main                     " + mainid);
-		//console.log("new main                 " + nmainid);
-		//console.log("pocket                   " + pocketid);
-		//console.log("new pocket               " + npocketid);
-		//console.log("pocketParent             " + pocketPid);
-		//console.log("FirstPrimaryWeapon       " + FPWid);
-		//console.log("new FirstPrimaryWeapon   " + nFPWid);
-		//console.log("FirstPrimaryWeaponParent " + FPWPid);
-		//console.log("TacticalVest             " + TVid);
-		//console.log("new TacticalVest         " + nTVid);
-		//console.log("TacticalVest Parent      " + TVPid);
-		//console.log("ArmorVest                " + AVid);
-		//console.log("new ArmorVest            " + nAVid);
-		//console.log("ArmorVest Parent         " + AVPid);
-		//console.log("Backpack                 " + BPid);
-		//console.log("new Backpack             " + nBPid);
-		//console.log("Backpack Parent          " + BPPid);
-		//console.log("Headwear                 " + HWid);
-		//console.log("new Headwear             " + nHWid);
-		//console.log("Headwear Parent          " + HWPid);
-		//console.log("FaceCover                " + FCid);
-		//console.log("new FaceCover            " + nFCid);
-		//console.log("FaceCover Parent         " + FCPid);
-		//console.log("Scabbard                 " + SCid);
-		//console.log("new Scabbard             " + nSCid);
-		//console.log("Scabbard Parent          " + SCPid);
 		
 		//Re prep bot file	
 		out2 += '{"_id": "' + UniqID + '", "aid": 0,"savage": null,"Info": {' + 
 		'"Nickname": "' + botName + '","LowerNickname": "","Side": "Savage","Voice": "' + voice + '","Level": 1,"Experience": 0,"RegistrationDate": 0,"GameVersion": "","AccountType": 0,"MemberCategory": 0,"lockedMoveCommands": false,"LastTimePlayedAsSavage": 0,' + 
 		'"Settings":{"Role": "' + botType + '","BotDifficulty": "' + botDiff + '","Experience": -1},"NeedWipe": false,"GlobalWipe": false,"NicknameChangeDate": 0},' + 
 		'"Customization": ' + prepareCustomization + ' ' + '"Health": ' + JSON.stringify(profileList.Health) + ',"Inventory": ' + prepareInventory2 + ',"Skills": {"Common": null,"Mastering": [],"Points": 0},"Stats": {"SessionCounters": {"Items": []},"OverallCounters": {"Items": []}},"Encyclopedia": null,"ConditionCounters": {"Counters": []}, "BackendCounters": {},"InsuredItems": []}';
-		//write new file
-		fs.writeFileSync('randomz.json', out2, 'utf8');
+		
+		//write new file (new lines possible after each?)
+		outJson2 = ((outJson2 != "")?(outJson2 + ","):"") + out2;
+		fs.writeFileSync('randomz.json', outJson2, 'utf8');
+}
 	
 }
 console.log("-> Finished Creation of " + count + " bots.")
